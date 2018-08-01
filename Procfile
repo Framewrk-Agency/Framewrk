f72d103ed5d8ded7ada0bbb24d522b07cf467515
toddbirchard <toddbirchard@gmail.com>
2018-08-01 00:38:29 -0400

    changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --chdir framewrkapp app:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -9,13 +9,11 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 # Initiate apps
-app = Flask(__name__, static_url_path='/static', static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder="static", template_folder="templates",)
 
 # Static Files
 app.config['COMPRESSOR_DEBUG'] = app.config.get('DEBUG')
-app.config['COMPRESSOR_OUTPUT_DIR'] = 'build'
-app.config['COMPRESSOR_STATIC_PREFIX'] = 'static'
-app.static_folder = 'static'
+app.config['COMPRESSOR_STATIC_PREFIX'] = '/static'
 compress = FlaskStaticCompress(app)
 
 
