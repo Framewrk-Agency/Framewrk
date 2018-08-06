dda6e190b2ae45e16a2646521525b606dc1028e8
toddbirchard <toddbirchard@gmail.com>
2018-08-06 01:24:21 -0400

    draggable

--- static/js/dragdrop.js
+++ static/js/dragdrop.js
@@ -1,6 +1,27 @@
 $( document ).ready(function() {
+
+
+  function handleDragStop( event, ui ) {
+    var offsetXPos = parseInt( ui.offset.left );
+    var offsetYPos = parseInt( ui.offset.top );
+    alert( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
+  }
+
+  function handleDropEvent( event, ui ) {
+    alert(this + " got dropped");
+  }
+
+
   $('.shape').draggable({
     cursor: 'grabbing',
-    'snap': 'dropzone',
+    snap: '.dropzone',
+    revert: "invalid",
+    snapTolerance: 20,
+    snapMode: "inner",
+    stop: handleDragStop
+  });
+
+  $('.dropzone').droppable( {
+    tolerance: "intersect"
   });
 });
