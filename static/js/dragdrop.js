870b79d9a0bf4e3bb18b05d551ba11d2be384651
Todd Birchard <toddbirchard@gmail.com>
2018-08-07 01:55:44 -0400

    cleanup

--- static/js/dragdrop.js
+++ static/js/dragdrop.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
 
   var selection = '';
+  $('[data-toggle="tooltip"]').tooltip({placement: "bottom"});
 
   function handleDragStop( event, ui ) {
     var offsetXPos = parseInt( ui.offset.left );
@@ -8,22 +9,46 @@ $( document ).ready(function() {
     console.log( "Drag stopped!\n\nOffset: (" + offsetXPos + ", " + offsetYPos + ")\n");
   }
 
-  function handleDropEvent( event, ui ) {
-    selection = $(this).attr('data-type');
-    console.log(selection);
-  }
+
 
 
   $('.shape').draggable({
     cursor: 'grabbing',
     snap: '.dropzone',
     revert: "invalid",
-    snapTolerance: 20,
-    snapMode: "inner",
-    stop: handleDragStop
+    //snapTolerance: 20,
+    snapMode: "inner"
   });
 
   $('.dropzone').droppable( {
-    tolerance: "intersect"
+    tolerance: "intersect",
+    addClasses: true,
+    hoverClass: "drop-hover",
+    drop: function( event, ui ) {
+      selection = $(this).attr('data-type');
+      $( ".shape" ).position({
+        my: "center",
+        at: "center",
+        of: this
+      });
+    },
+    over: function( event, ui ) {
+      $(this).closest( "li" ).addClass('label-highlight');
+      $(this).next().addClass('hr-highlight');
+    },
+    out: function( event, ui ) {
+      $(this).closest( "li" ).removeClass('label-highlight');
+      $(this).next().removeClass('hr-highlight');
+      $(this).next().removeClass('hr-selected');
+      $(this).closest( "li" ).removeClass('label-selected');
+      $('.nextquestion').removeClass('nextquestion-active');
+    },
+    drop: function( event, ui ) {
+      $(this).next().removeClass('hr-highlight');
+      $(this).next().addClass('hr-selected');
+      $(this).closest( "li" ).removeClass('label-highlight');
+      $(this).closest( "li" ).addClass('label-selected');
+      $('.nextquestion').addClass('nextquestion-active');
+    }
   });
 });
